8a99e21589e4ff59984b621990a79013cd618772
youness kazize <younny7kaziz@gmail.com>
2022-10-23 14:38:49 +0200

    Adding files

---
+++ README.txt
@@ -0,0 +1,5 @@
+Upload local file into a container on Azure Cloud.
+
+This script will allow you to upload your local file in a container on Azure, 
+if you have more than one or two files you can modify the function by inserting a for loop which iterates over the files.
+You can contact if you have any doubt.
---
+++ Uploading file from local in azure.py
@@ -0,0 +1,26 @@
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+import sys
+from sys import path
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, dir_path)
+
+def Upload_file_on_Azure(connection_str,storage_account_name, container_name, local_file_name):
+    """
+    Upload_file_on_Azure function.
+        it upload file from local in Azure cloud.
+    Parameter:
+     connection_str => Your azure connection string.
+     storage_account_name => The name of your azure storage account.
+     container_name => the name of your container in azure.
+     local_file_name => The path of your local file.
+    Return:
+     uploading to Azure Storage as blob + local file name.
+    
+    """
+    upload_file_path = os.path.join(container_name, local_file_name)
+    blob_service_client = BlobServiceClient.from_connection_string(connection_str)
+    blob_client = blob_service_client.get_blob_client(container=storage_account_name, blob=local_file_name)
+    print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
+    with open(upload_file_path, "rb") as data:
+        blob_client.upload_blob(data)
